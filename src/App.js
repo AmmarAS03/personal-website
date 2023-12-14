f65f1303230cb4bed67987a2a04fd143f5bf353f
AmmarAS03 <ammar.ash@ui.ac.id>
2023-12-15 00:02:14 +0700

    add project file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import './App.scss';
-import Navbar from './components/Navbar';
-import Profile from './components/Profile';
-import AboutMe from './components/AboutMe';
-import WorkExperience from './components/WorkExperience';
-
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+import AboutMe from "./components/AboutMe";
+import WorkExperience from "./components/WorkExperience";
+import Project from "./components/Project";
 
 function App() {
   return (
-    <div className='App'>
-
-        <Navbar/>
-        <div className='separator'/>
-        <Profile/>
-        <div className='separator'/>
-        <AboutMe/>
-        <div className='separator'/>
-        <WorkExperience/>
-
+    <div className="App">
+      <Navbar />
+      <div className="separator" />
+      <Profile />
+      <div className="separator" />
+      <AboutMe />
+      <div className="separator" />
+      <WorkExperience />
+      <div className="separator" />
+      <Project/>
     </div>
   );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../assets/scss/Project.scss";
+
+function Project() {
+  return (
+    <div className='hello-heading-1'>Project</div>
+  )
+}
+
+export default Project
